dbe7eef5bff40e1ab9ce9de5452f6b4f392135db
Maciej Maciejewski <maciej.maciejewski@intel.com>
2018-04-30 10:17:03 +0200

    mist:keys structure cfg read handling

--- apps/mist/mist.cpp
+++ apps/mist/mist.cpp
@@ -1,31 +1,59 @@
-#include "deps/config4cpp/include/config4cpp/Configuration.h"
 #include <iostream>
-using namespace config4cpp;
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include <libconfig.h++>
+
+using namespace libconfig;
 using namespace std;
 
-int main(int argc, char ** argv)
-{
-  Configuration *  cfg = Configuration::create();
-  const char * mode;
-  const char * configFile = "mist.cfg";
-  const char * pmem_path;
-  uint64 pmem_size = 0;
+int main(int argc, char ** argv) {
+	Config cfg;
+	const char * configFile = "mist.cfg";
 
-  try {
-    cfg->parse(configFile);
-    mode = cfg->lookupString(scope, "mode");
-    pmem_path = cfg->lookupString(scope, "pmem_path");
-    pmem_size = cfg->lookupBoolean(scope, "pmem-size");
-  } 
-  catch(const ConfigurationException & ex) {
-    cerr << ex.c_str() << endl;
-    cfg->destroy();
-    return 1;
-  }
+	try {
+		cfg.readFile(configFile);
+	}
+	catch(const FileIOException &fioex) {
+		std::cerr << "I/O error while reading file." << std::endl;
+		return -1;
+	}
+	catch(const ParseException &pex) {
+		std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+        		      << " - " << pex.getError() << std::endl;
+		return -1;
+	}
 
-  cout << "mode=" << mode << "; fille=" << pmem_path
-	  << "; size=" << pmem_size
-	  << endl;
-  cfg->destroy();
-  return 0;
+	// required parameters
+	string mode;
+	int port;
+	cfg.lookupValue("mode", mode);
+	cfg.lookupValue("port", port);
+
+	string primaryHash, replicaHash, primaryKey;
+	vector<int> keysStructure;
+	cfg.lookupValue("primaryHash", primaryHash);
+	cfg.lookupValue("replicaHash", replicaHash);
+	const Setting &keys_settings = cfg.lookup("keys_structure");
+	for (int n = 0; n < keys_settings.getLength(); ++n)
+		keysStructure.push_back(keys_settings[n]);
+	cfg.lookupValue("primaryKey", primaryKey);
+
+	// optional parameters
+	string pmem_path = "";
+	int pmem_size = 0;
+	string loggingLevel = "WARN";
+	cfg.lookupValue("pmem_path", pmem_path);
+	cfg.lookupValue("pmem_size", pmem_size);
+	cfg.lookupValue("logging_level", loggingLevel);
+
+	cout << "FogKV/mode=" << mode << "; file=" << pmem_path
+				<< "; size=" << pmem_size
+				<< endl;
+	cout << "keys structure=";
+	for (auto n: keysStructure)
+		cout << n << " ";
+	cout << endl;
+
+	return 0;
 }
