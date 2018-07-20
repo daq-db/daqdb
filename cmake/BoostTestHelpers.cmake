2b1e46523003b4ce2fee87d4963281f0cc36d586
Jakub Radtke <jakub.radtke@intel.com>
2018-07-20 03:47:08 -0400

    spdk offload: update cmake to see BOOST_FIXTURE_TEST_CASE
    Signed-off-by: Jakub Radtke <jakub.radtke@intel.com>

--- cmake/BoostTestHelpers.cmake
+++ cmake/BoostTestHelpers.cmake
@@ -10,11 +10,22 @@ function(add_boost_test SOURCE_FILE_NAME)
 	file(READ "${SOURCE_FILE_NAME}" SOURCE_FILE_CONTENTS)
 	string(REGEX MATCHALL "BOOST_AUTO_TEST_CASE\\( *([A-Za-z_0-9]+) *\\)" 
 		FOUND_TESTS ${SOURCE_FILE_CONTENTS})
-
+	
 	foreach(HIT ${FOUND_TESTS})
 		string(REGEX REPLACE ".*\\( *([A-Za-z_0-9]+) *\\).*" "\\1" TEST_NAME ${HIT})
 
 		add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}" 
 			COMMAND ${TEST_EXECUTABLE_NAME} --run_test=${TEST_NAME} --log_level=all --catch_system_error=yes)
 	endforeach()
+
+	string(REGEX MATCHALL "BOOST_FIXTURE_TEST_CASE\\(([A-Za-z_0-9]+), [A-Za-z_0-9:]+\\)" 
+		FOUND_TESTS_F ${SOURCE_FILE_CONTENTS})
+
+	foreach(HIT ${FOUND_TESTS_F})
+		string(REGEX REPLACE "BOOST_FIXTURE_TEST_CASE\\(([A-Za-z_0-9]+), [A-Za-z_0-9:]+\\).*" "\\1" TEST_NAME ${HIT})
+
+		add_test(NAME "${TEST_EXECUTABLE_NAME}.${TEST_NAME}" 
+			COMMAND ${TEST_EXECUTABLE_NAME} --run_test=${TEST_NAME} --log_level=all --catch_system_error=yes)
+	endforeach()
+
 endfunction()
