b875fe93243b73e029dbc476b35bec8d33054e5a
Grzegorz Jereczek <grzegorz.jereczek@intel.com>
2018-09-28 07:50:33 +0200

    Adding eRPC to build system

--- third-party/erpc.cmake
+++ third-party/erpc.cmake
@@ -2,13 +2,21 @@ cmake_minimum_required(VERSION 3.5)
 
 include(ExternalProject)
 
-include_directories(spdk/dpdk/build/include)
 ExternalProject_Add(project_erpc
     PREFIX ${PROJECT_SOURCE_DIR}/eRPC
     SOURCE_DIR ${PROJECT_SOURCE_DIR}/eRPC
     BUILD_IN_SOURCE ${PROJECT_SOURCE_DIR}/eRPC
-    CMAKE_ARGS -DPERF=OFF -DTRANSPORT=dpdk
+    CMAKE_ARGS -DPERF=ON -DTRANSPORT=dpdk -DLTO=OFF -DCMAKE_PREFIX_PATH=${DPDK_PATH}
 	BUILD_COMMAND make
 	INSTALL_COMMAND ""
-    DEPENDS dpdk
+    DEPENDS project_spdk
+)
+add_library(liberpc STATIC IMPORTED GLOBAL)
+set_target_properties(liberpc PROPERTIES IMPORTED_LOCATION
+    ${PROJECT_SOURCE_DIR}/liberpc.a)
+add_dependencies(liberpc project_erpc)
+
+add_custom_target(liberpc_clean
+	COMMAND make clean
+    WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/eRPC
 )
