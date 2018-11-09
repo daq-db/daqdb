b8fd4e01a53971da7c8eb24417f24197f05dcbcc
Grzegorz Jereczek <grzegorz.jereczek@intel.com>
2018-11-09 12:51:49 +0100

    minidaq: changing key layout (#194)

--- apps/minidaq/MinidaqNode.h
+++ apps/minidaq/MinidaqNode.h
@@ -29,9 +29,9 @@ struct MinidaqKey {
     MinidaqKey() : eventId(0), subdetectorId(0), runId(0){};
     MinidaqKey(uint64_t e, uint16_t s, uint16_t r)
         : eventId(e), subdetectorId(s), runId(r) {}
-    uint64_t eventId;
     uint16_t subdetectorId;
     uint16_t runId;
+    uint64_t eventId;
 };
 
 class MinidaqNode {
