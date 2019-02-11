246879170a08307a3f7bae8b0175288dd05cb64a
Jakub Radtke <jakub.radtke@intel.com>
2019-02-11 13:42:01 +0100

    Update SPDK initialization (eRPC requires more hugepages) (#258)
    Signed-off-by: Jakub Radtke <jakub.radtke@intel.com>

--- scripts/setup_env_lcg.sh
+++ scripts/setup_env_lcg.sh
@@ -15,4 +15,5 @@ cvmfs_config probe
 
 source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_93 x86_64-centos7-gcc7-opt
 
-sudo $SCRIPT_PATH/../$SPDKPATH/scripts/setup.sh
+# sRPC requires at least 1024 of 2M hugepages (2048 for 2 sockets platform)
+sudo HUGEMEM=2048 $SCRIPT_PATH/../$SPDKPATH/scripts/setup.sh
