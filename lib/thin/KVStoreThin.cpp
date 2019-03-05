df497ac981f4423f6c3d53801538718558857edf
Jakub Radtke <jakub.radtke@intel.com>
2019-03-05 11:34:26 -0500

    Update daqdb_thin and functest_thin to work with DPDK transport
    Signed-off-by: Jakub Radtke <jakub.radtke@intel.com>

--- lib/thin/KVStoreThin.cpp
+++ lib/thin/KVStoreThin.cpp
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 #include "KVStoreThin.h"
@@ -39,6 +39,10 @@ KVStoreThin::~KVStoreThin() {}
 void KVStoreThin::init() {
     if (getOptions().runtime.logFunc)
         gLog.setLogFunc(getOptions().runtime.logFunc);
+
+#ifdef DPDK
+    _spSpdk.reset(new SpdkCore(getOptions().offload));
+#endif
 
     _spDht.reset(new DhtCore(getOptions().dht));
     _spDht->initNexus();
