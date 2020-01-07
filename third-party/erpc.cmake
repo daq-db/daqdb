c5af9110a42546060d820a920bdd57e19865fd88
Norbert Szulc <norbert.szulc@intel.com>
2020-01-07 12:45:44 +0100

    Remove eRPC-fork submodule

--- third-party/erpc.cmake
+++ third-party/erpc.cmake
@@ -5,9 +5,9 @@ include(ExternalProject)
 set(DPDK_PATH ${PROJECT_SOURCE_DIR}/spdk/dpdk/build)
 
 ExternalProject_Add(project_erpc
-	PREFIX ${PROJECT_SOURCE_DIR}/eRPC-fork
-	SOURCE_DIR ${PROJECT_SOURCE_DIR}/eRPC-fork
-	BUILD_IN_SOURCE ${PROJECT_SOURCE_DIR}/eRPC-fork
+	PREFIX ${PROJECT_SOURCE_DIR}/eRPC
+	SOURCE_DIR ${PROJECT_SOURCE_DIR}/eRPC
+	BUILD_IN_SOURCE ${PROJECT_SOURCE_DIR}/eRPC
 	CMAKE_ARGS -DPERF=ON -DTRANSPORT=${ERPC_TRANSPORT_MODE} -DLTO=OFF -DCMAKE_PREFIX_PATH=${DPDK_PATH} -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 	BUILD_COMMAND ${CMAKE_MAKE_PROGRAM}
 	INSTALL_COMMAND ""
@@ -15,5 +15,5 @@ ExternalProject_Add(project_erpc
 )
 add_library(liberpc STATIC IMPORTED GLOBAL)
 set_target_properties(liberpc PROPERTIES IMPORTED_LOCATION
-    ${PROJECT_SOURCE_DIR}/eRPC-fork/build/liberpc.a)
+    ${PROJECT_SOURCE_DIR}/eRPC/build/liberpc.a)
 add_dependencies(liberpc project_erpc)
